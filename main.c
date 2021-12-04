dce06d1085ba585c22235da48d9f0d1758033863
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-12-04 15:04:19 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int i = 7;
+	for(int x = 0; x < 100; x++) {
+		printf("%d ", i);
+		if(i % 2 == 0) {
+			i /= 2;
+		}
+		else {
+			i /= 3;
+			i++;
+		}
+	}
+	return 0;
+}
